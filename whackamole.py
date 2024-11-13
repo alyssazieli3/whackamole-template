9c39f5382042fd01ab07b040d1ad2f876e068590
alyssazieli3 <alyssateal3@gmail.com>
2024-11-13 17:47:21 -0500

    Add files via upload

--- whackamole.py
+++ whackamole.py
@@ -1,4 +1,9 @@
 import pygame
+import math
+
+#set constants
+height = 512
+width = 640
 
 
 def main():
@@ -15,6 +20,22 @@ def main():
                 if event.type == pygame.QUIT:
                     running = False
             screen.fill("light green")
+            #draw horizontal lines
+            for i in range(1, 16):
+                pygame.draw.line(
+                    screen,
+                    "black",
+                    (0, i * width / 20),
+                    (width, i * width / 20))
+            #draw vertical lines
+            for i in range(1, 20):
+                pygame.draw.line(
+                    screen,
+                    "black",
+                     (i* width/20, 0),
+                     (i*width/20, height)
+                )
+
             pygame.display.flip()
             clock.tick(60)
     finally:
