1c580089c5a92f6fc7e186a0dac24333eaaac5dd
alyssazieli3 <alyssateal3@gmail.com>
2024-11-14 14:33:41 -0500

    Add files via upload
    Final Push

--- whackamole.py
+++ whackamole.py
@@ -1,9 +1,28 @@
 import pygame
 import math
+import random
 
 #set constants
 height = 512
 width = 640
+
+def set_screen(screen):
+    screen.fill("light green")
+    # draw horizontal lines
+    for i in range(1, 16):
+        pygame.draw.line(
+            screen,
+            "purple",
+            (0, i * width / 20),
+            (width, i * width / 20))
+    # draw vertical lines
+    for i in range(1, 20):
+        pygame.draw.line(
+            screen,
+            "purple",
+            (i * width / 20, 0),
+            (i * width / 20, height)
+        )
 
 
 def main():
@@ -15,26 +34,27 @@ def main():
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+
+        set_screen(screen)
+
+        #set mole
+        screen.blit(mole_image, mole_image.get_rect(topleft = (0, 0)))
+        mole_pos = [0, 0]
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
-            #draw horizontal lines
-            for i in range(1, 16):
-                pygame.draw.line(
-                    screen,
-                    "black",
-                    (0, i * width / 20),
-                    (width, i * width / 20))
-            #draw vertical lines
-            for i in range(1, 20):
-                pygame.draw.line(
-                    screen,
-                    "black",
-                     (i* width/20, 0),
-                     (i*width/20, height)
-                )
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = event.pos
+                    x = x // 32
+                    y = y // 32
+                    if [x, y] == mole_pos:
+                        new_x = random.randrange(0, 20)
+                        new_y = random.randrange(0, 16)
+                        set_screen(screen)
+                        screen.blit(mole_image, mole_image.get_rect(topleft = (new_x*32, new_y*32)))
+                        mole_pos = [new_x, new_y]
 
             pygame.display.flip()
             clock.tick(60)
